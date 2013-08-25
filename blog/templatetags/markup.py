ba9be266d744d2ef4bc750359a9fad7e551fee93
James Bennett <james@b-list.org>
2013-08-25 12:34:36 -0500

    Do that the smart way.

--- blog/templatetags/markup.py
+++ blog/templatetags/markup.py
@@ -2,7 +2,8 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
-from blog.models import markup
+from markdown import markdown
+from typogrify.filters import typogrify
 
 
 register = template.Library()
@@ -11,5 +12,12 @@ register = template.Library()
 @register.filter
 @stringfilter
 def markup(value):
-    result = markup(value)
+    result = typogrify(markdown(text,
+                                lazy_ol=False,
+                                output_format='html5',
+                                extensions=['abbr',
+                                            'codehilite',
+                                            'fenced_code',
+                                            'sane_lists',
+                                            'smart_strong']))
     return mark_safe(result)
