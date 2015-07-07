cf9701c7c73bb11297fb4d995e2bae04a3171944
James Bennett <james@b-list.org>
2015-07-06 23:18:49 -0700

    Modernize URLs.

--- blog/urls/categories.py
+++ blog/urls/categories.py
@@ -3,16 +3,15 @@ URLs for categories in the blog.
 
 """
 
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from blog import views
 
-urlpatterns = patterns('',
-                       url(r'^$',
-                           views.CategoryList.as_view(),
-                           name='blog_category_list'),
-                       url(r'^(?P<slug>[-\w]+)/$',
-                           views.CategoryDetail.as_view(),
-                           name='blog_category_detail'),
-)
+urlpatterns = [
+    url(r'^$',
+        views.CategoryList.as_view(),
+        name='blog_category_list'),
+    url(r'^(?P<slug>[-\w]+)/$',
+        views.CategoryDetail.as_view(),
+        name='blog_category_detail'),
+]
