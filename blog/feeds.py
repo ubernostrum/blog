297485d510fcc6e98bd891bba238d7ce526c46a0
James Bennett <james@b-list.org>
2018-03-27 00:27:52 -0700

    Clean up the feeds a bit more.

--- blog/feeds.py
+++ blog/feeds.py
@@ -12,14 +12,15 @@ current_site = Site.objects.get_current()
 
 class EntriesFeed(Feed):
     author_name = "James Bennett"
-    copyright = "http://www.b-list.org/about/copyright/"
-    description = "Latest entries posted to %s" % current_site.name
+    copyright = "https://{}/about/copyright/".format(current_site.domain)
+    description = "Latest entriess"
     feed_type = Atom1Feed
-    item_copyright = "http://www.b-list.org/about/copyright/"
+    item_copyright = "https://{}/about/copyright/".format(current_site.domain)
     item_author_name = "James Bennett"
-    item_author_link = "http://www.b-list.org/"
-    link = "/feeds/entries/"
-    title = "%s: Latest entries" % current_site.name
+    item_author_link = "https://{}/".format(current_site.domain)
+    feed_url = "https://{}/feeds/entries/".format(current_site.domain)
+    link = "https://{}/".format(current_site.domain)
+    title = "James Bennett (b-list.org)"
 
     description_template = 'feeds/entry_description.html'
     title_template = 'feeds/entry_title.html'
@@ -28,9 +29,11 @@ class EntriesFeed(Feed):
         return [c.title for c in item.categories.all()]
 
     def item_guid(self, item):
-        return "tag:%s,%s:%s" % (current_site.domain,
-                                 item.pub_date.strftime('%Y-%m-%d'),
-                                 item.get_absolute_url())
+        return "tag:{},{}:{}".format(
+            current_site.domain,
+            item.pub_date.strftime('%Y-%m-%d'),
+            item.get_absolute_url()
+    )
 
     def item_pubdate(self, item):
         return item.pub_date
@@ -41,11 +44,23 @@ class EntriesFeed(Feed):
     def items(self):
         return Entry.live.all()[:15]
 
+    def item_link(self, item):
+        return "https://{}{}".format(
+            current_site.domain,
+            item.get_absolute_url()
+        )
+
 
 class CategoryFeed(EntriesFeed):
+    def feed_url(self, obj):
+        return "https://{}/feeds/categories/{}/".format(
+            current_site.domain, obj.slug
+        )
+
     def description(self, obj):
-        return "%s: Latest entries in category '%s'" % (current_site.name,
-                                                        obj.title)
+        return "Latest entries in category '{}'".format(
+            obj.title
+        )
 
     def get_object(self, request, slug):
         return Category.objects.get(slug=slug)
@@ -54,8 +69,9 @@ class CategoryFeed(EntriesFeed):
         return obj.live_entries[:15]
 
     def link(self, obj):
-        return obj.get_absolute_url()
+        return self.item_link(obj)
 
     def title(self, obj):
-        return "%s: Latest entries in category '%s'" % (current_site.name,
-                                                        obj.title)
+        return "Latest entries in category '{}'".format(
+            obj.title
+        )
